9aa6e8aab9bdc9ea3e4c8d232e498c2d8eb34f69
Ashraf Monshi <wh7r.com@gmail.com>
2019-06-05 03:08:55 +0300

    more styling

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,4 +6,8 @@ require('laravel-mix-purgecss');
 mix.js('resources/js/app.js', 'public/js')
    .postCss('resources/css/app.css', 'public/css')
    .tailwind('./tailwind.config.js')
-   .purgeCss();
+   .purgeCss({
+       enabled: true,
+       // Other options are passed through to Purgecss
+       whitelistPatterns: [/hljs/ , /ql-syntax/],
+   });
