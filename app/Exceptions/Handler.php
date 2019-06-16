432009c0812c1c371e06e87e5c3fdfcd034358e4
Ashraf Monshi <wh7r.com@gmail.com>
2019-06-16 05:04:57 +0300

    add sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,47 +5,58 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
-class Handler extends ExceptionHandler
-{
+class Handler extends ExceptionHandler {
+
     /**
      * A list of the exception types that are not reported.
      *
      * @var array
      */
-    protected $dontReport = [
-        //
-    ];
+    protected $dontReport
+        = [
+            \Symfony\Component\HttpKernel\Exception\NotFoundHttpException::class ,
+            \Illuminate\Validation\ValidationException::class ,
+            \Illuminate\Auth\AuthenticationException::class ,
+            \Illuminate\Auth\Access\AuthorizationException::class ,
+            \Symfony\Component\HttpKernel\Exception\HttpException::class ,
+            \Illuminate\Database\Eloquent\ModelNotFoundException::class ,
+        ];
 
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
      * @var array
      */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
+    protected $dontFlash
+        = [
+            'password' ,
+            'password_confirmation' ,
+        ];
 
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param \Exception $exception
      * @return void
      */
     public function report(Exception $exception)
     {
+        if(app()->bound('sentry') && $this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
+
         parent::report($exception);
     }
 
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param \Illuminate\Http\Request $request
+     * @param \Exception $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+    public function render($request , Exception $exception)
     {
-        return parent::render($request, $exception);
+        return parent::render($request , $exception);
     }
 }
